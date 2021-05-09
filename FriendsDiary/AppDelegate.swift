fd5d57acd9c00acaf16d7fee99836cd2899bb045
jisungbin <ji@sungb.in>
2021-05-10 03:25:32 +0900

    널 위해 공부하는 중

---
+++ FriendsDiary/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  FriendsDiary
+//
+//  Created by Ji Sungbin on 2021/05/10.
+//
+
+import SwiftUI
+
+import KakaoSDKCommon
+import KakaoSDKAuth
+
+
+class AppDelegate: NSObject, UIApplicationDelegate{
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        KakaoSDKCommon.initSDK(appKey: "3dec89acaf5e4025a1c5763500a88c41", loggingEnable:false)
+
+        return true
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+           if (AuthApi.isKakaoTalkLoginUrl(url)) {
+               return AuthController.handleOpenUrl(url: url, options: options)
+           }
+        
+           return false
+       }
+}
--- FriendsDiary/ContentView.swift
+++ FriendsDiary/ContentView.swift
@@ -6,11 +6,53 @@
 //
 
 import SwiftUI
+import KakaoSDKAuth
+import KakaoSDKUser
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("FriendsDiary")
+                .font(.system(size: 30, weight: .bold, design: .default))
+            Button(action : {
+                if (UserApi.isKakaoTalkLoginAvailable()) {
+                    UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
+                        if let error = error {
+                            print(error)
+                        }
+                        else {
+                            print("loginWithKakaoTalk() success.")
+                            _ = oauthToken
+                        }
+                    }
+                } else {
+                    UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
+                        if let error = error {
+                            print(error)
+                        }
+                        else {
+                            print("loginWithKakaoAccount() success.")
+                            _ = oauthToken
+                        }
+                    }
+                }
+            }){
+                Text("카카오 로그인으로 시작하기")
+                    .padding(10.0)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(lineWidth: 2)
+                            .shadow(color: .gray, radius: 10)
+                    )
+                    .foregroundColor(.gray)
+                    .padding(.top, 50)
+            }
+            .onOpenURL(perform: { url in
+                if (AuthApi.isKakaoTalkLoginUrl(url)) {
+                    _ = AuthController.handleOpenUrl(url: url)
+                }
+            })
+        }
     }
 }
 
